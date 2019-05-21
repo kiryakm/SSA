8497aeb60d0f7bf9dfb5110c279d25055d42555f
kiryakm <yakimech@gmail.com>
2019-05-21 20:03:10 +0700

    updated ssa and example

--- ssaCore.py
+++ ssaCore.py
@@ -1,93 +1,96 @@
 import numpy as np
-import scipy.linalg as linalg
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-import os
-import operator
+import scipy.linalg as linalg
+from scipy import linalg
 
-def ts(x):
-    """
-    Усредняем побочные диоганали элементарной матрицы 
-    и переводим в временной ряд
-    """
-    xrev = x[::-1]
-    return np.array([xrev.diagonal(i).mean() for i in range(-x.shape[0]+1, x.shape[1])])
-
-def nans(dims):
-    """
-    nans((M,N,P,...)) is an M-by-N-by-P-by-... array of NaNs.
-    :param dims: dimensions tuple
-    :return: nans matrix
-    """
-    return np.nan * np.ones(dims)
-
-class SSA:
-    def __init__(self):
-        self.components = []
-
-    def ssa(self, F, L):
+class mySSA():
+    
+    def __init__(self, F):        
         self.F = F
-        self.L = L
         self.N = len(F)
-        self.K = self.N - self.L - 1
+
+    def ts(self, Xi):
+        """
+        Усредняем побочные диоганали элементарной матрицы 
+        и переводим в временной ряд
+        """
+        Xrev = Xi[::-1]
+        return np.array([Xrev.diagonal(i).mean() for i in range(-Xi.shape[0]+1, Xi.shape[1])])
+
+    def getContributions(self):
+        """
+        Рассчитать относительный вклад каждого из сингулярных значений
+        """
+        lambdas = np.power(self.s,2)
+        norm = np.linalg.norm(self.X)
+        cont = [(lambdas[i]/(norm**2)).round(4) for i in range(len(self.s))]
+        cont = {i:cont[i] for i in range(len(cont)) if cont[i]>0}
+        return cont
+
+    def decompose(self, L):
+        """
+        Создаем 
+        """
+        self.L = L    
+        self.K = self.N - L + 1
 
         self.X = np.column_stack([self.F[i:i+self.L] for i in range(0,self.K)])
         self.d = np.linalg.matrix_rank(self.X) 
-        self.U, self.sigma, self.V = np.linalg.svd(self.X)
+        self.U, self.s, self.V = np.linalg.svd(self.X)
         self.V = self.V.T 
-        self.Xe = np.array([self.sigma[i] * \
+        self.Xe = np.array([self.s[i] * \
             np.outer(self.U[:,i], self.V[:,i]) for i in range(0, self.L)])
 
-    def getComponents(self, p=0.8):
-        for i in range(self.d):
-            for j in range(i+1, self.d):
-                stat = stats.pearsonr(ts(self.Xe[i]), ts(self.Xe[j]))[0]
-                if stat >= p:
-                    self.components.extend((i,j))
+        self.sContributions = self.getContributions()
+        self.r = len(self.sContributions)
+        self.orthonormalBase = {i:self.U[:,i] for i in range(self.r)}
+    
+        
+    def getComponents(self, p = 0.8):
+        self.components = []        
+        for i, j in zip(np.arange(self.d)[::2], np.arange(self.d)[1::2]):
+            stat = stats.pearsonr(self.ts(self.Xe[i]), self.ts(self.Xe[j]))[0]
+            if stat >= p:
+                self.components.extend((i,j))
         if 0 not in self.components:
             self.components.append(0)
         if 1 not in self.components:
             self.components.append(1)
 
-    def filterComponents(self, p=0.04):   
-        for i, j in zip(self.components[0::2], self.components[1::2]):
-            percent = (self.sigma[i] + self.sigma[i+1])\
-                / sum(self. sigma)
-            if percent < p:
-                self.components.pop(self.components.index(i))
-                self.components.pop(self.components.index(j))
+    def filterComponents(self, p = 0.0004):   
+        keys = []
+        self.components = []
+        for key in self.sContributions:
+            keys.append(key)
+        for i, j in zip(keys[0::2], keys[1::2]):
+            pers = self.sContributions[i] + self.sContributions[j]
+            if pers >= p:
+                self.components.append(i)
+                self.components.append(j)
+    
+    def reconstruct(self):
+        Xs = np.array([self.ts(self.Xe[i]) for i in self.components])
+        self.tsRec = np.zeros(len(Xs[0]))
+        for i in range(len(Xs)):
+            self.tsRec += Xs[i]
+        return self.tsRec
 
-    def restore(self):
-        self.Xf = np.array([ts(self.Xe[i]) for i in self.components])
-        self.Xf = sum(self.Xf)
-        
-
-    def predict(self, nForecast, e=None, maxIter=10000):
-        if not e:
-            e = 0.01 * (np.max(self.F) - np.min(self.F))
-        Xmean = self.F.mean()
-        x = self.F - Xmean
-        self.Xfor = nans(nForecast)
-
-
-        for i in range(nForecast):
-            x = np.append(x, x[-1])
-            yq = x[-1]
-            y = yq + 2 * e
-            nIter = maxIter
-            while abs(y - yq) > e:
-                yq = x[-1]
-            
-                self.ssa(x, self.L)
-                xr = np.array([ts(self.Xe[i]) for i in self.components])
-                xr = sum(xr)
-
-                y = xr[-1]
-                x[-1] = y
-                nIter -= 1
-                if nIter <= 0:
-                    print('number of iterations exceeded')
-                    break
-
-            self.Xfor[i] = x[-1]
-        self.Xfor = self.Xfor + Xmean
+    def forecastPrep(self):
+        self.verticalityCoeff = 0
+        self.R = np.zeros(self.orthonormalBase[0].shape)[:-1]
+        for Pi in self.orthonormalBase.values():
+            pi = np.ravel(Pi)[-1]
+            self.verticalityCoeff += pi**2
+            self.R += pi*Pi[:-1]
+        self.R = np.matrix(self.R/(1-self.verticalityCoeff))
+    
+    def forecast(self, steps):
+        self.forecastPrep()
+        self.tsForecast = self.tsRec
+        for i in range(self.N + steps):
+            if i >= self.N:
+                Z = np.array([self.tsForecast[j] for j in range(i-self.L, i-1)])
+                x = self.R @ Z
+                self.tsForecast = np.append(self.tsForecast, x)
+        return self.tsForecast
